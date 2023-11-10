d62f99093c6e22d6e1fdaf0a46e8199af050411f
chenshuai <chenshuai@ian2018.cn>
2023-11-10 22:40:03 +0800

    finish ext

--- scripts/swapper.py
+++ scripts/swapper.py
@@ -3,58 +3,57 @@ from dataclasses import dataclass
 from typing import Union, Dict
 
 from PIL import Image
+import tempfile
 
 from facefusion.uis.components import output
+
+from facefusion.core import apply_args, get_argument_parser, conditional_process, pre_check
+from facefusion.processors.frame.modules import face_enhancer, face_swapper, frame_enhancer
+import facefusion.globals
 
 
 @dataclass
 class ImageResult:
-    path: Union[str, None] = None
-    similarity: Union[Dict[int, float], None] = None  # face, 0..1
+	path: Union[str, None] = None
 
-    def image(self) -> Union[Image.Image, None]:
-        if self.path:
-            return Image.open(self.path)
-        return None
+	def image(self) -> Union[Image.Image, None]:
+		if self.path:
+			return Image.open(self.path)
+		return None
 
 
 def swap_face(
-    source_img: Image.Image,
-    target_img: Image.Image,
+	source_img: Image.Image,
+	target_img: Image.Image,
 	image_quality: int = 80,
 ) -> ImageResult:
-    result_image = target_img
-    converted = convert_to_sd(target_img)
-    scale, fn = converted[0], converted[1]
-    if model is not None and not scale:
-        if isinstance(source_img, str):  # source_img is a base64 string
-            import base64, io
-            if 'base64,' in source_img:  # check if the base64 string has a data URL scheme
-                base64_data = source_img.split('base64,')[-1]
-                img_bytes = base64.b64decode(base64_data)
-            else:
-                # if no data URL scheme, just decode
-                img_bytes = base64.b64decode(source_img)
-            source_img = Image.open(io.BytesIO(img_bytes))
-        source_img = cv2.cvtColor(np.array(source_img), cv2.COLOR_RGB2BGR)
-        target_img = cv2.cvtColor(np.array(target_img), cv2.COLOR_RGB2BGR)
-        source_face = get_face_single(source_img, face_index=0)
-        if source_face is not None:
-            result = target_img
-            model_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), model)
-            face_swapper = getFaceSwapModel(model_path)
-
-            for face_num in faces_index:
-                target_face = get_face_single(target_img, face_index=face_num)
-                if target_face is not None:
-                    result = face_swapper.get(result, target_face, source_face)
-                else:
-                    logger.info(f"No target face found for {face_num}")
-
-            result_image = Image.fromarray(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
-            if upscale_options is not None:
-                result_image = upscale_image(result_image, upscale_options)
-        else:
-            logger.info("No source face found")
-    result_image.save(fn.name)
-    return ImageResult(path=fn.name)
+	if isinstance(source_img, str):  # source_img is a base64 string
+		import base64, io
+		if 'base64,' in source_img:  # check if the base64 string has a data URL scheme
+			base64_data = source_img.split('base64,')[-1]
+			img_bytes = base64.b64decode(base64_data)
+		else:
+			# if no data URL scheme, just decode
+			img_bytes = base64.b64decode(source_img)
+		source_img = Image.open(io.BytesIO(img_bytes))
+	apply_args(get_argument_parser())
+	limit_resources()
+	if not pre_check():
+		return ImageResult()
+	if (
+		not face_enhancer.pre_check()
+		or not face_swapper.pre_check()
+		or not frame_enhancer.pre_check()
+	):
+		return ImageResult()
+	source_path = tempfile.NamedTemporaryFile(delete=False, suffix=".png").name
+	source_img.save(source_path)
+	target_path = tempfile.NamedTemporaryFile(delete=False, suffix=".png").name
+	target_img.save(target_path)
+	facefusion.globals.source_path = source_path
+	facefusion.globals.target_path = target_path
+	facefusion.globals.output_path = tempfile.NamedTemporaryFile(delete=False, suffix=".png").name
+	facefusion.globals.output_image_quality = image_quality
+	facefusion.globals.frame_processors = ['face_swapper', 'face_enhancer']
+	conditional_process()
+	return ImageResult(path=facefusion.globals.output_path)
