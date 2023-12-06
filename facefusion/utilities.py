68778cfebe4bba19e747b802990390b671ff71db
chenshuai <chenshuai@amberweather.com>
2023-12-06 11:34:20 +0800

    add log

--- facefusion/utilities.py
+++ facefusion/utilities.py
@@ -144,6 +144,7 @@ def encode_execution_providers(execution_providers : List[str]) -> List[str]:
 def decode_execution_providers(execution_providers: List[str]) -> List[str]:
 	available_execution_providers = onnxruntime.get_available_providers()
 	encoded_execution_providers = encode_execution_providers(available_execution_providers)
+	print(f'[ FACE_FUSION ] {execution_providers}')
 	return [ execution_provider for execution_provider, encoded_execution_provider in zip(available_execution_providers, encoded_execution_providers) if any(execution_provider in encoded_execution_provider for execution_provider in execution_providers) ]
 
 
