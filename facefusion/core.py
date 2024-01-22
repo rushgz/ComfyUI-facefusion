e56b6db0212d6e4739ef4f7155c2bc7171776729
chenshuai <chenshuai@amberweather.com>
2024-01-22 10:22:05 +0800

    merge facefusion 2.2.1

--- facefusion/core.py
+++ facefusion/core.py
@@ -2,34 +2,31 @@ import os
 
 os.environ['OMP_NUM_THREADS'] = '1'
 
-import ssl
 import sys
+import time
 import warnings
-import platform
 import shutil
+import numpy
 import onnxruntime
+from time import sleep
 
 import facefusion.choices
 import facefusion.globals
 from facefusion.face_analyser import get_one_face, get_average_face
 from facefusion.face_store import get_reference_faces, append_reference_face
-from facefusion.vision import read_image, read_static_images
-from facefusion import face_analyser, face_masker, content_analyser, logger, wording
+from facefusion import face_analyser, face_masker, content_analyser, config, metadata, logger, wording
 from facefusion.content_analyser import analyse_image
 from facefusion.processors.frame.core import get_frame_processors_modules
 from facefusion.execution_helper import decode_execution_providers
-from facefusion.normalizer import normalize_output_path
-from facefusion.filesystem import is_image
-import facefusion.globals
+from facefusion.normalizer import normalize_output_path, normalize_padding, normalize_fps
+from facefusion.memory import limit_system_memory
+from facefusion.filesystem import list_directory, get_temp_frame_paths, create_temp, move_temp, clear_temp, is_image, is_video
+from facefusion.vision import get_video_frame, read_image, read_static_images, pack_resolution, detect_video_resolution, detect_video_fps, create_video_resolutions
 from facefusion.processors.frame import globals as frame_processors_globals
 
 onnxruntime.set_default_logger_severity(3)
 warnings.filterwarnings('ignore', category = UserWarning, module = 'gradio')
 warnings.filterwarnings('ignore', category = UserWarning, module = 'torchvision')
-
-
-if platform.system().lower() == 'darwin':
-	ssl._create_default_https_context = ssl._create_unverified_context
 
 
 def apply_args(source_path, target_path, output_path, provider, detector_score, mask_blur, skip_nsfw) -> None:
@@ -51,9 +48,11 @@ def apply_args(source_path, target_path, output_path, provider, detector_score, 
 		providers = decode_execution_providers(['cpu'])
 	facefusion.globals.execution_providers = providers
 	logger.info(f"device use {facefusion.globals.execution_providers}", __name__.upper())
-	facefusion.globals.execution_thread_count = 1
+	facefusion.globals.execution_thread_count = 4
 	facefusion.globals.execution_queue_count = 1
-	facefusion.globals.max_memory = None
+	# memory
+	facefusion.globals.video_memory_strategy = 'strict'
+	facefusion.globals.system_memory_limit = 0
 	# face analyser
 	facefusion.globals.face_analyser_order = 'large-small'
 	facefusion.globals.face_analyser_age = None
@@ -76,7 +75,7 @@ def apply_args(source_path, target_path, output_path, provider, detector_score, 
 	facefusion.globals.output_image_quality = 100
 	# frame processors
 	facefusion.globals.frame_processors = ['face_swapper', 'face_enhancer']
-	frame_processors_globals.face_swapper_model = "inswapper_128"
+	frame_processors_globals.face_swapper_model = "inswapper_128_fp16"
 	facefusion.globals.face_recognizer_model = 'arcface_inswapper'
 	frame_processors_globals.face_enhancer_model = 'gfpgan_1.4'
 	frame_processors_globals.face_enhancer_blend = 100
@@ -85,7 +84,8 @@ def apply_args(source_path, target_path, output_path, provider, detector_score, 
 def run(source_path, target_path, output_path, provider="cpu", detector_score=0.6, mask_blur=0.3, skip_nsfw=True):
 	apply_args(source_path, target_path, output_path, provider, detector_score, mask_blur, skip_nsfw)
 	logger.init(facefusion.globals.log_level)
-	limit_resources()
+	if facefusion.globals.system_memory_limit > 0:
+		limit_system_memory(facefusion.globals.system_memory_limit)
 	if not pre_check() or not face_analyser.pre_check() or not face_masker.pre_check():
 		return None
 	if not skip_nsfw and not content_analyser.pre_check():
@@ -99,22 +99,6 @@ def run(source_path, target_path, output_path, provider="cpu", detector_score=0.
 	return None
 
 
-def limit_resources() -> None:
-	if facefusion.globals.max_memory:
-		memory = facefusion.globals.max_memory * 1024 ** 3
-		if platform.system().lower() == 'darwin':
-			memory = facefusion.globals.max_memory * 1024 ** 6
-		if platform.system().lower() == 'windows':
-			import ctypes
-
-			kernel32 = ctypes.windll.kernel32 # type: ignore[attr-defined]
-			kernel32.SetProcessWorkingSetSize(-1, ctypes.c_size_t(memory), ctypes.c_size_t(memory))
-		else:
-			import resource
-
-			resource.setrlimit(resource.RLIMIT_DATA, (memory, memory))
-
-
 def pre_check() -> bool:
 	if sys.version_info < (3, 9):
 		logger.error(wording.get('python_not_supported').format(version = '3.9'), __name__.upper())
@@ -123,12 +107,17 @@ def pre_check() -> bool:
 
 
 def conditional_process() -> None:
-	conditional_append_reference_faces()
+	start_time = time.time()
 	for frame_processor_module in get_frame_processors_modules(facefusion.globals.frame_processors):
+		while not frame_processor_module.post_check():
+			logger.disable()
+			sleep(0.5)
+		logger.enable()
 		if not frame_processor_module.pre_process('output'):
 			return
+	conditional_append_reference_faces()
 	if is_image(facefusion.globals.target_path):
-		process_image()
+		process_image(start_time)
 
 
 def conditional_append_reference_faces() -> None:
@@ -141,13 +130,15 @@ def conditional_append_reference_faces() -> None:
 			append_reference_face('origin', reference_face)
 			if source_face and reference_face:
 				for frame_processor_module in get_frame_processors_modules(facefusion.globals.frame_processors):
-					reference_frame = frame_processor_module.get_reference_frame(source_face, reference_face,
-																				 reference_frame)
-					reference_face = get_one_face(reference_frame, facefusion.globals.reference_face_position)
-					append_reference_face(frame_processor_module.__name__, reference_face)
+					abstract_reference_frame = frame_processor_module.get_reference_frame(source_face, reference_face,
+																						  reference_frame)
+					if numpy.any(abstract_reference_frame):
+						reference_frame = abstract_reference_frame
+						reference_face = get_one_face(reference_frame, facefusion.globals.reference_face_position)
+						append_reference_face(frame_processor_module.__name__, reference_face)
 
 
-def process_image() -> None:
+def process_image(start_time : float) -> None:
 	if (not facefusion.globals.skip_nsfw) and analyse_image(facefusion.globals.target_path):
 		logger.info(f"skip process, source image is nsfw", "CORE")
 		return
@@ -167,6 +158,7 @@ def process_image() -> None:
 
 	# validate image
 	if is_image(facefusion.globals.output_path):
-		logger.info(wording.get('processing_image_succeed'), __name__.upper())
+		seconds = '{:.2f}'.format((time.time() - start_time) % 60)
+		logger.info(wording.get('processing_image_succeed').format(seconds=seconds), __name__.upper())
 	else:
 		logger.error(wording.get('processing_image_failed'), __name__.upper())
