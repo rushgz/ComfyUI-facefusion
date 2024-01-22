1adc93dedfd26d28381b6c38a9e8c48f42024abb
chenshuai <chenshuai@amberweather.com>
2024-01-22 10:40:57 +0800

    update log formate

--- facefusion/logger.py
+++ facefusion/logger.py
@@ -1,31 +1,61 @@
+import copy
+import logging
+import sys
+from logging import getLogger, Logger, DEBUG, INFO, WARNING, ERROR
 from typing import Dict
-from logging import basicConfig, getLogger, Logger, DEBUG, INFO, WARNING, ERROR
 
 from facefusion.typing import LogLevel
 
 
-def init(log_level : LogLevel) -> None:
-	basicConfig(format = None)
-	get_package_logger().setLevel(get_log_levels()[log_level])
+class ColoredFormatter(logging.Formatter):
+	COLORS = {
+		"DEBUG": "\033[0;36m",  # CYAN
+		"INFO": "\033[0;32m",  # GREEN
+		"WARNING": "\033[0;33m",  # YELLOW
+		"ERROR": "\033[0;31m",  # RED
+		"CRITICAL": "\033[0;37;41m",  # WHITE ON RED
+		"RESET": "\033[0m",  # RESET COLOR
+	}
+
+	def format(self, record):
+		colored_record = copy.copy(record)
+		levelname = colored_record.levelname
+		seq = self.COLORS.get(levelname, self.COLORS["RESET"])
+		colored_record.levelname = f"{seq}{levelname}{self.COLORS['RESET']}"
+		return super().format(colored_record)
+
+
+def init(log_level: LogLevel) -> None:
+	logger = get_package_logger()
+	logger.propagate = False
+
+	# Add handler if we don't have one.
+	if not logger.handlers:
+		handler = logging.StreamHandler(sys.stdout)
+		handler.setFormatter(
+			ColoredFormatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+		)
+		logger.addHandler(handler)
+	logger.setLevel(get_log_levels()[log_level])
 
 
 def get_package_logger() -> Logger:
 	return getLogger('facefusion')
 
 
-def debug(message : str, scope : str) -> None:
+def debug(message: str, scope: str) -> None:
 	get_package_logger().debug('[' + scope + '] ' + message)
 
 
-def info(message : str, scope : str) -> None:
+def info(message: str, scope: str) -> None:
 	get_package_logger().info('[' + scope + '] ' + message)
 
 
-def warn(message : str, scope : str) -> None:
+def warn(message: str, scope: str) -> None:
 	get_package_logger().warning('[' + scope + '] ' + message)
 
 
-def error(message : str, scope : str) -> None:
+def error(message: str, scope: str) -> None:
 	get_package_logger().error('[' + scope + '] ' + message)
 
 
@@ -38,10 +68,10 @@ def disable() -> None:
 
 
 def get_log_levels() -> Dict[LogLevel, int]:
-	return\
-	{
-		'error': ERROR,
-		'warn': WARNING,
-		'info': INFO,
-		'debug': DEBUG
-	}
+	return \
+		{
+			'error': ERROR,
+			'warn': WARNING,
+			'info': INFO,
+			'debug': DEBUG
+		}
